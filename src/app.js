268845548ad34f9fbbf0bad9234b8f5a95525eec
Gattik <gattiktyagi7@gmail.com>
2026-05-19 09:49:50 +0530

    feat: add GET endpoint for fetching tasks

--- src/app.js
+++ src/app.js
@@ -3,3 +3,12 @@ const app=express();
 
 app.use(express.json());
 module.exports = app;
+
+
+const tasks = [];
+app.get('/tasks',(req,res)=>{
+    res.status(201).json({
+        "message":"All tasks fetched successfully",
+        "task":tasks
+    });
+})
