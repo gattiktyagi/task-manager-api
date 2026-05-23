bba5ab79729f7c74bbcd8b7cdacb4728ddc5d258
Gattik <gattiktyagi7@gmail.com>
2026-05-23 10:26:34 +0530

    feat: add PATCH endpoint to update existing tasks by ID

--- src/app.js
+++ src/app.js
@@ -29,3 +29,22 @@ app.delete('/tasks/:id',(req,res)=>{
     })
 
 })
+
+app.patch('/tasks/:id',(req,res)=>{
+    let taskId=Number(req.params.id);
+    let title=req.body.title;
+    let description=req.body.description;
+
+    if(!tasks[taskId]){
+        return res.status(404).json({
+            "message":"Task not found"
+        })
+    }
+
+    if(title!==undefined)tasks[taskId].title=title;
+    if(description!==undefined)tasks[taskId].description=description;
+    res.status(200).json({
+        "message":"Task updated successfully",
+        "Updated Task":tasks[taskId]
+    })
+})
