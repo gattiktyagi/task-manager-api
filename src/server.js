95a4c8a871669ebf2b9117364f73b1d11c209dae
Gattik <gattiktyagi7@gmail.com>
2026-05-25 16:44:13 +0530

    feat: set up database connection and create initial server structure

--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
 const app = require('./app');
-const PORT = 3000;
+const pool = require('./config/db');
 
-app.listen(PORT,()=>{
-    console.log(`Server is running at PORT ${PORT}`);
-});
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+async function startServer() {
+    try{
+        await pool.query("select 1");
+        console.log("Database Connected Successfully");
+
+        app.listen(PORT,()=>{
+            console.log(`Server is running on port ${PORT}`);
+        });
+    }
+    catch(error){
+        console.error("Database Connection Error");
+        console.error(error);
+    }
+}
+startServer();
